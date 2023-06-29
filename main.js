20e3ef6f83b7710c6d10fe189bbec4fd91dd76ae
devonriley <devontriley@gmail.com>
2023-06-29 18:47:28 -0400

    Conditionally load Paradox chat script

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-<!-- Chatbot -->
+const allowedDomains = [ 'vinfen.org', 'vinfenct.org' ];
 
-var oliviaChatData = oliviaChatData || [];
-var oliviaChatBaseUrl =  'https://olivia.paradox.ai';
-oliviaChatData.push(['setKey', 'mytbbsrvgnbyqtaelvcx']);
-oliviaChatData.push(['start']);
-(function() {
-    var apply = document.createElement('script');
-    apply.type = 'text/javascript';
-    apply.async = true;
-    apply.src = 'https://dokumfe7mps0i.cloudfront.net/static/site/js/widget-client.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(apply, s);
-})();
+if ( allowedDomains.some(domain => window.location.origin.includes(domain)) && window.location.href.includes('work-at-vinfen') ) {
+    var oliviaChatData = oliviaChatData || [];
+    var oliviaChatBaseUrl =  'https://olivia.paradox.ai';
+    oliviaChatData.push(['setKey', 'mytbbsrvgnbyqtaelvcx']);
+    oliviaChatData.push(['start']);
+    (function() {
+        var apply = document.createElement('script');
+        apply.type = 'text/javascript';
+        apply.async = true;
+        apply.src = 'https://dokumfe7mps0i.cloudfront.net/static/site/js/widget-client.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(apply, s);
+    })();
+}
