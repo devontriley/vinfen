3b69848db065423e1ca680e8dfede9f0408ec0c2
devonriley <devontriley@gmail.com>
2023-06-29 13:23:41 -0400

    Remove hard coded GTM script

--- functions.php
+++ functions.php
@@ -9,34 +9,6 @@ if ( wp_get_environment_type() !== 'production' ) {
     add_action( 'wp_head', 'load_bugherd' );
 }
 
-// Add Google Tag Manager to head
-function append_gtm_header () { ?>
-    <!-- Google Tag Manager -->
-    <script>
-        (function(w,d,s,l,i){
-            w[l]=w[l]||[];
-            w[l].push({
-                'gtm.start': new Date().getTime(),event:'gtm.js'
-            });
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TDSV2NZ');
-    </script>
-    <!-- End Google Tag Manager -->
-<?php }
-add_action( 'wp_head', 'append_gtm_header' );
-
-// Add Google Tag Manger to body
-function append_gtm_body () { ?>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDSV2NZ" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-<?php }
-add_action( 'wp_body_open', 'append_gtm_body' );
-
 if (!function_exists('vinfen_enqueue_styles')) :
     function vinfen_enqueue_styles() {
         wp_enqueue_style( 'vinfen-style', get_stylesheet_uri(), array( 'heretic-style' ), filemtime(get_stylesheet_directory() . '/style.css') );
