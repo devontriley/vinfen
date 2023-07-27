25fff8191d244e29c329c29e0c5c6364445a49a6
devonriley <devontriley@gmail.com>
2023-07-27 07:56:26 -0400

    Fixing PHP warning

--- inc/vinfen-hooks.php
+++ inc/vinfen-hooks.php
@@ -5,6 +5,8 @@
  */
 function vinfen_footer_logo( $logo ) {
     $footerLogo = get_field( 'footer_logo', 'option' );
-    return wp_get_attachment_image( $footerLogo['ID'], 'full' ) ?? $logo;
+    if ( is_array($footerLogo) ) {
+        return wp_get_attachment_image( $footerLogo['ID'], 'full' ) ?? $logo;
+    }
 }
 add_filter( 'heretic_footer_logo', 'vinfen_footer_logo' );
