d969dd3b1e361988f181e4dca36f2b3090269a6b
devonriley <devontriley@gmail.com>
2023-08-03 14:06:19 -0400

    Remove bugherd script

--- functions.php
+++ functions.php
@@ -1,14 +1,5 @@
 <?php
 
-// Enable Bughed on development
-if ( wp_get_environment_type() !== 'production' ) {
-    function load_bugherd() { ?>
-        <!-- Bugherd -->
-        <script type="text/javascript" src="https://www.bugherd.com/sidebarv2.js?apikey=xunwkfarbvamuqrpcanvxq" async="true"></script>
-    <?php }
-    add_action( 'wp_head', 'load_bugherd' );
-}
-
 if (!function_exists('vinfen_enqueue_styles')) :
     function vinfen_enqueue_styles() {
         wp_enqueue_style( 'vinfen-style', get_stylesheet_uri(), array( 'heretic-style' ), filemtime(get_stylesheet_directory() . '/style.css') );
